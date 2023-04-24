3cdaa010be66760b36702edaa833628944d979bc
Risu <79110363+risu729@users.noreply.github.com>
2023-04-24 19:27:22 +0900

    Add attachments to log (#34)

--- src/main/java/io/github/risu729/erutcurts/misc/Notifications.java
+++ src/main/java/io/github/risu729/erutcurts/misc/Notifications.java
@@ -10,15 +10,21 @@ package io.github.risu729.erutcurts.misc;
 
 import com.google.common.base.Throwables;
 import io.github.risu729.erutcurts.Erutcurts;
+import io.github.risu729.erutcurts.util.Attachments;
 import io.github.risu729.erutcurts.util.EmbedUtil;
 import io.github.risu729.erutcurts.util.Envs;
+import io.github.risu729.erutcurts.util.file.CloseablePath;
+import io.github.risu729.erutcurts.util.file.FileUtil;
 import java.awt.Color;
+import java.util.Collection;
+import java.util.List;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
+import net.dv8tion.jda.api.utils.FileUpload;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -37,13 +43,22 @@ public class Notifications {
   }
 
   public void sendLog(@NotNull String message, @Nullable Guild guild, @NotNull User user) {
+    sendLog(message, guild, user, List.of());
+  }
+
+  public void sendLog(
+      @NotNull String message,
+      @Nullable Guild guild,
+      @NotNull User user,
+      @NotNull Collection<? extends Message.@NotNull Attachment> attachments) {
     sendToNotificationChannel(
         EmbedUtil.createDefaultBuilder("Log")
             .setColor(Color.GREEN)
             .setDescription(message)
             .addField("Guild", guild == null ? "DM" : guild.getName(), false)
             .addField("User", user.getAsTag(), false)
-            .build());
+            .build(),
+        attachments);
   }
 
   public void replyStackTrace(@NotNull IReplyCallback callback, @NotNull Throwable throwable) {
@@ -75,8 +90,24 @@ public class Notifications {
   }
 
   private void sendToNotificationChannel(@NotNull MessageEmbed embed) {
-    Erutcurts.getMessageChannelById(Envs.getEnv("NOTIFICATION_CHANNEL_ID"))
-        .sendMessageEmbeds(embed)
-        .queue();
+    sendToNotificationChannel(embed, List.of());
+  }
+
+  private void sendToNotificationChannel(
+      @NotNull MessageEmbed embed,
+      @NotNull Collection<? extends Message.@NotNull Attachment> attachments) {
+    var action =
+        Erutcurts.getMessageChannelById(Envs.getEnv("NOTIFICATION_CHANNEL_ID"))
+            .sendMessageEmbeds(embed);
+    try (var closeableTempDir = CloseablePath.of(FileUtil.createTempDir())) {
+      action
+          .addFiles(
+              Attachments.download(attachments, closeableTempDir.path(), true).stream()
+                  .map(FileUpload::fromData)
+                  .toList())
+          .queue();
+    } catch (RuntimeException e) {
+      action.queue();
+    }
   }
 }
